c7fe6125f2bd816d84c30b94833e3625e8913d85
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-06-01 21:15:42 -0300

    Realocados os arquivos do projeto. Adicionado o .yml para o CircleCI

---
+++ src/main/java/es3team/wordhang/controller/GameController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es3team.wordhang.controller;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author 631420063
+ */
+@ManagedBean(eager = true)
+@ApplicationScoped
+public class GameController {
+
+    private String[] letras;
+
+    /**
+     * Creates a new instance of TestController
+     */
+    public GameController() {
+        this.letras = new String[]{"A", "B", "C", "D", "E", "F",
+        "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+        "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    }
+
+    public String[] getLetras() {
+        return letras;
+    }
+}
